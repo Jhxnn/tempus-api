7d3cea46cd96df34d01a2d0b4acd7aaf82235a97
Hilixx <jhonalizan@gmail.com>
2025-07-03 13:12:47 -0300

    updatePoint

--- src/main/java/com/tempus_api/services/PointService.java
+++ src/main/java/com/tempus_api/services/PointService.java
@@ -1,6 +1,7 @@
 package com.tempus_api.services;
 
 import com.tempus_api.dtos.PointDto;
+import com.tempus_api.models.Employee;
 import com.tempus_api.models.Point;
 import com.tempus_api.repositories.PointRepository;
 import org.springframework.beans.BeanUtils;
@@ -16,12 +17,35 @@ public class PointService {
     @Autowired
     PointRepository pointRepository;
 
+    @Autowired
+    EmployeeService employeeService;
+
     public Point createPoint(PointDto pointDto){
         Point point = new Point();
         BeanUtils.copyProperties(pointDto, point);
-
+        Employee employee = employeeService.findById(pointDto.employeeId());
+        point.setEmployee(employee);
         return pointRepository.save(point);
     }
+
+    public Point updatePoint(PointDto pointDto, UUID id){
+        Point point = findById(id);
+        if(pointDto.start() != null){
+            point.setStart(pointDto.start());
+        }
+        if(pointDto.finish() != null){
+            point.setFinish(pointDto.finish());
+        }
+        if(pointDto.date() != null){
+            point.setDate(pointDto.date());
+        }
+        if(pointDto.employeeId() != null){
+            Employee employee = employeeService.findById(pointDto.employeeId());
+            point.setEmployee(employee);
+        }
+        return pointRepository.save(point);
+    }
+
 
     public Point findById(UUID id){
         return pointRepository.findById(id).orElseThrow(() -> new RuntimeException("cannot be found"));
