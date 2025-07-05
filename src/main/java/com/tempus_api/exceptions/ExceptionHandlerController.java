49f070cba5051acf404cdb72a200de5281e5e334
Hilixx <jhonalizan@gmail.com>
2025-07-05 11:35:01 -0300

    exception handler

--- src/main/java/com/tempus_api/exceptions/ExceptionHandlerController.java
+++ src/main/java/com/tempus_api/exceptions/ExceptionHandlerController.java
@@ -1,8 +1,21 @@
 package com.tempus_api.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+
 public class ExceptionHandlerController extends RuntimeException {
 
-    public ExceptionHandlerController(String message) {
-        super(message);
+    @ExceptionHandler(ConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Map<String, String> handleConflict(ConflictException ex) {
+        return Map.of("error", ex.getMessage());
     }
-}
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleBadRequest(BadRequestException ex) {
+        return Map.of("error", ex.getMessage());
+    }}
