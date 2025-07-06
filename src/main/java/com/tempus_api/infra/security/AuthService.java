e99c39d3f826aaf05d2f6fcf3c9fdccdbc53ab2d
Hilixx <jhonalizan@gmail.com>
2025-07-06 12:26:11 -0300

    authservice

--- src/main/java/com/tempus_api/infra/security/AuthService.java
+++ src/main/java/com/tempus_api/infra/security/AuthService.java
@@ -1,4 +1,20 @@
 package com.tempus_api.infra.security;
 
-public class AuthService {
+import com.tempus_api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
 }
--- src/main/java/com/tempus_api/repositories/UserRepository.java
+++ src/main/java/com/tempus_api/repositories/UserRepository.java
@@ -2,8 +2,12 @@ package com.tempus_api.repositories;
 
 import com.tempus_api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    UserDetails findByEmail(String email);
+
 }
