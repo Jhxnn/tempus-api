db89e7fbe32b57d599f710acb16747bb6e996a66
Hilixx <jhonalizan@gmail.com>
2025-07-09 22:53:41 -0300

    userController swagger

--- src/main/java/com/tempus_api/controllers/UserController.java
+++ src/main/java/com/tempus_api/controllers/UserController.java
@@ -5,27 +5,44 @@ import com.tempus_api.dtos.AuthResponseDto;
 import com.tempus_api.dtos.RegisterDto;
 import com.tempus_api.dtos.UserResponseDto;
 import com.tempus_api.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
-@RestController("/api/v1/user")
+@RestController
+@RequestMapping("/api/v1/user")
+@Tag(name = "Usuário", description = "Operações de autenticação e cadastro")
 public class UserController {
 
     @Autowired
     UserService userService;
 
+    @Operation(summary = "Realizar login do usuário")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Login realizado com sucesso"),
+            @ApiResponse(responseCode = "401", description = "Credenciais inválidas")
+    })
     @PostMapping("/login")
-    public ResponseEntity<AuthResponseDto> login(@RequestBody AuthDto authDto){
+    public ResponseEntity<AuthResponseDto> login(
+            @Parameter(description = "Credenciais do usuário") @RequestBody AuthDto authDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.login(authDto));
     }
 
+    @Operation(summary = "Registrar novo usuário")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Usuário registrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos")
+    })
     @PostMapping("/register")
-    public ResponseEntity<UserResponseDto> register(@RequestBody RegisterDto registerDto){
+    public ResponseEntity<UserResponseDto> register(
+            @Parameter(description = "Dados do novo usuário") @RequestBody RegisterDto registerDto) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.register(registerDto));
     }
 }
